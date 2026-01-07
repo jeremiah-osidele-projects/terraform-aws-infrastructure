157d8c0a42f10bb69d3a6ea54746a6d944d69b79
Jeremiah Ibukun Osidele <136078903+16ukun@users.noreply.github.com>
2026-01-07 21:52:38 +0000

    chore: configure aws s3 bucket and public access policy (#3)
    Authored-by: Jeremiah.Osidele <jeremiahosidele@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,22 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
+
+resource "aws_s3_bucket" "landing_page" {
+    bucket = "fight-tally-landing"
+
+    tags = {
+        Name = "FightTallyLanding"
+        Environment = "Dev"
+        ManagedBy = "Terraform"
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "landing_page" {
+    bucket = aws_s3_bucket.landing_page.id
+
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
