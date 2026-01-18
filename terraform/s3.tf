e39174bef311a6b2d3fa55ed467ae370ed7a92e5
Jeremiah.Osidele <Jeremiah.Osidele@fdmgroup.com>
2026-01-18 22:43:46 +0000

    chore: apply serverside enxryption with kms key to s3 landing page bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -16,3 +16,15 @@ resource "aws_s3_bucket_public_access_block" "landing_page" {
     ignore_public_acls = true
     restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "landing_page" {
+    bucket = aws_s3_bucket.landing_page.id
+
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+
+        kms_master_key_id = aws_kms_key.s3_encryption.arn
+      }
+    }
+}
